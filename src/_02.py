9035c22965ac42f76ecdf7a53e4320dbd9cf668c
subhandp <subhan.dinda.putra@gmail.com>
2020-09-10 18:01:30 +0800

    02 none

--- src/_02.py
+++ src/_02.py
@@ -14,41 +14,36 @@ def cli():
 @click.argument("city",type=str, nargs=-1)
 
 def weathers(cities,city):
-    if cities:
-        print(city)
-        
+    cities_param = city
+    list_task = []
+    async def multi_weather(cities_param):
+        for city in cities_param:
+            cor = get_coord(city)
+            if len(cor):
+                print(cor[0]["coord"]["lat"])
+            else:
+                print('nothing')
 
-# cities = ["jakarta","bandung","surabaya"]
-# list_task = []
-
-# async def multi_weather(cities):
-#     for city in cities:
-#         cor = get_coord(city)
-#         if len(cor):
-#             print(cor[0]["coord"]["lat"])
-#         else:
-#             print('nothing')
-
-#         lat = str(cor[0]["coord"]["lat"])
-#         lon = str(cor[0]["coord"]["lon"])
+            lat = str(cor[0]["coord"]["lat"])
+            lon = str(cor[0]["coord"]["lon"])
 
 
-#         task = asyncio.create_task(get_current_weather_async(lat,lon))
+            task = asyncio.create_task(get_current_weather_async(lat,lon))
 
-#         # weather = await task
-#         # print(weather)
-#         list_task.append(task)
+            # weather = await task
+            # print(weather)
+            list_task.append(task)
 
-#     rets = await asyncio.gather(*list_task)
-#     print(rets)
+        rets = await asyncio.gather(*list_task)
+        print(rets)
 
-#     # for task in list_task:
-#     #     weather = await task
-#     #     print(list(weather))
+        # for task in list_task:
+        #     weather = await task
+        #     print(list(weather))
 
 
 
-# if __name__ == "__main__":
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(multi_weather(cities))
+    if __name__ == "__main__":
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(weathers(cities,city))
 
